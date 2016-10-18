fb8e01ff7783396d21d451673e5fa75e8207d059
pratas <pratas@ua.pt>
2016-10-18 11:06:19 +0100

    info

--- run/run.sh
+++ run/run.sh
@@ -49,7 +49,8 @@ if [[ "$RUN_TOP" -eq "1" ]]; then
   size=`ls -la data/out*.fa | wc -l`;
   for((x=1 ; x<=$size; ++x));
     do
-    bytes=`./GeCo -v -tm 4:1:0:0/0 -tm 6:1:1:0/0 -tm 13:20:1:0/0 -tm 16:20:1:2/10 -c 20 -g 0.9 data/out$x.fa | grep "Total bytes" | awk '{ print $16; }'`;
+    echo "Running $x out of $size ...";
+    bytes=`./GeCo -tm 4:1:0:0/0 -tm 6:1:1:0/0 -tm 13:20:1:0/0 -tm 16:20:1:2/10 -c 20 -g 0.9 data/out$x.fa | grep "Total bytes" | awk '{ print $16; }'`;
     name=`cat data/out$x.fa | grep ">"`;
     printf "%s\t%s\n" "$bytes" "$name" >> TOP
     done
