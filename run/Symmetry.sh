d872c0449b06c1957635faa7c251a704f72f7dcf
pratas <pratas@ua.pt>
2016-12-07 15:06:35 +0000

    minor

--- run/Symmetry.sh
+++ run/Symmetry.sh
@@ -46,20 +46,20 @@ fi
 # RUN XS
 if [[ "$RUN_XS" -eq "1" ]]; then
   rm -f SYM*;
-  for((x=1,y=1 ; x<100000 ; x*=10, ++y)); do
-  ./XS -v -ls 10000 -n $x -eh -eo -es -edb -f 0.25,0.25,0.25,0.25,0.0 -rn 0 -rm 0 -s 0 SYMX$y;
+  for((x=1,y=1 ; x<10000000 ; x*=10, ++y)); do
+  ./XS -v -ls 1000 -n $x -eh -eo -es -edb -f 0.25,0.25,0.25,0.25,0.0 -rn 0 -rm 0 -s 0 SYMX$y;
   ./goose-mutatedna -mr 0.10 < SYMX$y > SYMY$y;
   done
 fi
 #==============================================================================
 # RUN GECO
 if [[ "$RUN_GECO" -eq "1" ]]; then
-  for((x=1 ; x<6 ; ++x)); do
+  for((x=1 ; x<8 ; ++x)); do
     echo "Sequence $x:";
     cat SYMX$x SYMY$x > XY$x;
     cat SYMY$x SYMX$x > YX$x;
-    ./GeCo -tm 4:1:0:0/0 -tm 6:1:1:0/0 -tm 13:20:1:0/0 -tm 16:20:1:2/10 -c 30 -g 0.9 XY$x;
-    ./GeCo -tm 4:1:0:0/0 -tm 6:1:1:0/0 -tm 13:20:1:0/0 -tm 16:20:1:2/10 -c 30 -g 0.9 YX$x;
+    ./GeCo -tm 4:1:0:0/0 -tm 6:1:1:0/0 -tm 13:20:1:0/0 -tm 18:20:1:2/10 -c 30 -g 0.9 XY$x;
+    ./GeCo -tm 4:1:0:0/0 -tm 6:1:1:0/0 -tm 13:20:1:0/0 -tm 18:20:1:2/10 -c 30 -g 0.9 YX$x;
   done
 fi
 #==============================================================================
